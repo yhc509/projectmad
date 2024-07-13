81852c54a1e2fb88932b7755971e0ca24bd4454c
anah2run <44317284+anah2run@users.noreply.github.com>
2024-07-13 21:33:56 +0900

    리소스 몇가지

--- Assets/doowon/Scripts/EnemyController.cs
+++ Assets/doowon/Scripts/EnemyController.cs
@@ -6,10 +6,12 @@ public class EnemyFollow : MonoBehaviour
 {
     public Transform target; // Player's transform
     private NavMeshAgent agent;
+    public Animator animator;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -17,6 +19,18 @@ public class EnemyFollow : MonoBehaviour
         if (target != null)
         {
             agent.SetDestination(target.position);
+        }
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (animator != null)
+        {
+            if(target != null)
+            {
+                animator.SetLookAtWeight(1f);
+                animator.SetLookAtPosition(target.position);
+            }
         }
     }
 }
