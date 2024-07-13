7fcef579d2687cd01c0e19c11db8c88beb9f14e4
anah2run <44317284+anah2run@users.noreply.github.com>
2024-07-14 00:22:19 +0900

    배경 리소스 추가

--- Assets/doowon/Scripts/EnemyController.cs
+++ Assets/doowon/Scripts/EnemyController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,11 +8,16 @@ public class EnemyFollow : MonoBehaviour
     public Transform target; // Player's transform
     private NavMeshAgent agent;
     public Animator animator;
+    Vector2 smoothDeltaPosition = Vector2.zero;
+    Vector2 velocity = Vector2.zero;
+    public float crawlThreshold = 0.5f;
+    bool crawl = false;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        agent.updatePosition = false;
     }
 
     void Update()
@@ -20,6 +26,42 @@ public class EnemyFollow : MonoBehaviour
         {
             agent.SetDestination(target.position);
         }
+
+        Vector3 worldDeltaPosition = agent.nextPosition - transform.position;
+
+        // Map 'worldDeltaPosition' to local space
+        float dx = Vector3.Dot(transform.right, worldDeltaPosition);
+        float dy = Vector3.Dot(transform.forward, worldDeltaPosition);
+        Vector2 deltaPosition = new Vector2(dx, dy);
+
+        // Low-pass filter the deltaMove
+        float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
+        smoothDeltaPosition = Vector2.Lerp(smoothDeltaPosition, deltaPosition, smooth);
+
+        // Update velocity if time advances
+        if (Time.deltaTime > 1e-5f)
+            velocity = smoothDeltaPosition / Time.deltaTime;
+
+        bool shouldMove = velocity.magnitude > 0.5f && agent.remainingDistance > agent.radius;
+
+        // Update animation parameters
+        float speed = velocity.magnitude;
+        animator.SetFloat("Speed", speed);
+        if(speed> crawlThreshold)
+        {
+            crawl = true;
+        }
+        if(crawl && speed < 0.5f)
+        {
+            crawl = false;
+        }
+        animator.SetBool("Crawl", crawl);
+    }
+
+    void OnAnimatorMove()
+    {
+        // Update position to agent position
+        transform.position = agent.nextPosition;
     }
 
     private void OnAnimatorIK(int layerIndex)
@@ -29,7 +71,8 @@ public class EnemyFollow : MonoBehaviour
             if(target != null)
             {
                 animator.SetLookAtWeight(1f);
-                animator.SetLookAtPosition(target.position);
+                //animator.SetLookAtPosition(agent.steeringTarget + transform.forward);
+                animator.SetLookAtPosition(target.transform.position);
             }
         }
     }
