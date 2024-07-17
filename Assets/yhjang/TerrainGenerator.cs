052e22a3b3c4b2606fd78a81c12b1c5375452809
yhc5091 <yhc5091@gmail.com>
2024-07-17 22:25:26 +0900

    onlyup

---
+++ Assets/yhjang/TerrainGenerator.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    public int width = 256;
+    public int height = 256;
+    public float scale = 20f;
+    public float heightMultiplier = 10f;
+
+    [ContextMenu("Generate")]
+    public void Generate()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+    }
+
+    TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, heightMultiplier, height);
+
+        terrainData.SetHeights(0, 0, GenerateHeights());
+        return terrainData;
+    }
+
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y);
+            }
+        }
+
+        return heights;
+    }
+
+    float CalculateHeight(int x, int y)
+    {
+        float xCoord = (float)x / width * scale;
+        float yCoord = (float)y / height * scale;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
